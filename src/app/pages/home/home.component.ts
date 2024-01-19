868cc55e74a1463b1c39f6a8ee93d0a1103ea257
João Paulo <joaowais@gmail.com>
2024-01-19 17:22:04 -0300

    criado weatherService

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from 'src/app/services/weather.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(private weatherService: WeatherService) {}
 
+  ngOnInit(): void {
+    this.weatherService.getWeatherData('Landon').subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+    });
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com/city',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+  XRapidAPIHostKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIHostKeyHeaderValue:
+    '63c93170c3msh96049abe56fe633p1a9aacjsn3edfa07dbbed',
+};
